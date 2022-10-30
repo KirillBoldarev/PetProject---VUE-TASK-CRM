f55b3919c217964c753136d40f309f389c3f00bd
KirillBoldarev <JQWOW@yandex.ru>
2022-10-30 23:43:23 +0300

    login form vuelidate refactor

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -4,49 +4,56 @@
     <p>Введите ваши персональные данные:</p>
     <form class="form" @submit.prevent="this.authenticateUser">
       <div class="form__group">
-        <label
-          v-if="this.invalidEmail === false"
-          class="form__label"
-          for="email"
-          >Электронная почта</label
-        >
-        <label
-          v-if="this.invalidEmail === true"
-          class="form__invalid"
-          for="email"
-          >Введена не верная электронная почта!</label
-        >
-        <input v-model="email" class="form__input" type="email" name="email" />
+        <div class="row">
+          <label class="form__label" for="email">Электронная почта</label>
+          <input
+            @blur="v$.email.$touch"
+            v-model="email"
+            class="form__input"
+            type="email"
+            name="email"
+          />
+        </div>
+        <div class="row">
+          <small
+            v-if="v$.email.$dirty && v$.email.required.$invalid"
+            class="invalidData"
+            >Поле обязательно для заполнения
+          </small>
+          <small
+            v-else-if="v$.email.$dirty && v$.email.email.$invalid"
+            class="invalidData"
+            >Некорректный формат электронной почты
+          </small>
+        </div>
       </div>
 
       <div class="form__group">
-        <label
-          v-if="this.invalidPassword === false"
-          class="form__label"
-          for="password"
-          >Пароль</label
-        >
-        <label
-          v-if="this.invalidPassword === true"
-          class="form__invalid"
-          for="password"
-          >Введен не верный пароль!</label
-        >
-        <input
-          v-model="password"
-          class="form__input"
-          type="password"
-          name="password"
-        />
+        <div class="row">
+          <label class="form__label" for="password">Пароль</label>
+          <input
+          @blur="v$.password.$touch"
+            v-model="password"
+            class="form__input"
+            type="password"
+            name="password"
+          />
+        </div>
+        <div class="row">
+          <small
+            v-if="v$.password.$dirty && v$.password.required.$invalid"
+            class="invalidData"
+            >Поле обязательно для заполнения
+          </small>
+        </div>
+      </div>
+
+      <div v-if="this.incorrectData === true" class="row invalidData">
+        Вы ввели не верный логин или пароль!
       </div>
 
       <div class="form__group">
-        <button
-          class="form__button"
-          type="submit"
-        >
-          Войти в систему
-        </button>
+        <button class="form__button" type="submit">Войти в систему</button>
         <a @click="this.$emit('changeToRegistration')" href="#" class="switch"
           >У меня нет аккаунта!</a
         >
@@ -56,7 +63,16 @@
 </template>
 
 <script>
+import { useVuelidate } from "@vuelidate/core";
+import { required, email, minLength } from "@vuelidate/validators";
+
 export default {
+  setup() {
+    return {
+      v$: useVuelidate(),
+    };
+  },
+
   components: {},
   name: "LoginForm",
 
@@ -70,8 +86,13 @@ export default {
     return {
       email: "",
       password: "",
-      invalidEmail: false,
-      invalidPassword: false,
+      incorrectData: false,
+    };
+  },
+  validations() {
+    return {
+      email: { required, email },
+      password: { required },
     };
   },
 
@@ -84,14 +105,27 @@ export default {
 
   methods: {
     authenticateUser() {
-      let foundedUser = this.userList.find((user) => user.email === this.email);
-      if (foundedUser == undefined) {
-        this.invalidEmail = true;
+      if (this.v$.$invalid) {
+        this.v$.$touch();
         return;
-      }
-      if (foundedUser.password !== this.password) {
-        this.invalidPassword = true;
       } else {
+        let foundedUser = this.userList.find(
+          (user) => user.email === this.email
+        );
+        if (foundedUser == undefined) {
+          this.incorrectData = true;
+          setTimeout(() => {
+            this.incorrectData = false;
+          }, 3000);
+          return;
+        }
+        if (foundedUser.password !== this.password) {
+          this.incorrectData = true;
+          setTimeout(() => {
+            this.incorrectData = false;
+          }, 3000);
+          return;
+        }
         this.$store.commit("authenticateCurrentUser", foundedUser);
         this.$emit("close");
       }
@@ -103,8 +137,6 @@ export default {
       }
     },
   },
-
-  computed: {},
 };
 </script>
 
@@ -129,7 +161,7 @@ h2 {
 
   &__group {
     display: flex;
-    flex-direction: row;
+    flex-direction: column;
     justify-content: space-between;
     align-items: center;
     gap: 15px;
@@ -145,5 +177,24 @@ h2 {
     color: red;
     text-decoration: underline;
   }
+}
+.row {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+  width: 100%;
+}
+.invalidData {
+  color: red;
+  font-size: 14px;
+  font-weight: 700;
+  text-decoration: underline;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
 }
 </style>
