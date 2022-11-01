efbe10bf5769fed75913922fb444cf85275a3bc5
KirillBoldarev <JQWOW@yandex.ru>
2022-11-01 12:02:27 +0300

    asd

--- src/store/modules/tasks.js
+++ src/store/modules/tasks.js
@@ -22,6 +22,11 @@ export default {
         state.taskList = taskList;
       }
     },
+
+    addTask(state, task) { 
+      state.taskList.push(task);
+      localStorage.setItem("taskList", JSON.stringify(taskList));
+    }
   },
   actions: {},
 };
