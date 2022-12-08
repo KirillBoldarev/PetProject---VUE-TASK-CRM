0c2c00247ec533ac636f6323519e22c08b003d74
Darina Syromyatnikova <d.syromyatnikova@darix.com>
2022-12-08 22:55:55 +0700

    build: add additional data to scss files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+          @import "@/src/styles/_var.scss";
+        `,
+      },
+    },
+  },
 });
