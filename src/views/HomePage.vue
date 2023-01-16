515a22eb298f4731dd3c534439d1a48f3af00701
KirillBoldarev <JQWOW@yandex.ru>
2023-01-16 11:18:38 +0300

    homePage refactor

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -17,49 +17,6 @@
         </button-with-modal-form>
         &nbsp;, если у вас есть учетная запись.
       </div>
-
-      <div>
-        <ul class="page__list">
-          Данный Pet-проект реализовывает:
-          <li class="page__list-item">CRUD-операции над пользователями</li>
-          <li class="page__list-item">
-            CRUD-операции над задачами, котороми обмениваются пользователи
-            системы
-          </li>
-        </ul>
-      </div>
-      <div>
-        <ul class="page__list">
-          В процессе разработки были применены инструменты:
-          <li class="page__list-item">VUE v.3 - как основной фреймворк</li>
-          <li class="page__list-item">
-            VUE-Router - для маршрутизации страниц приложения
-          </li>
-          <li class="page__list-item">
-            VUEX - для реализации глобального хранилища, обеспечивающего
-            рективное отображение данных, получаемых из Session Storage и
-            IndexedDB
-          </li>
-          <li class="page__list-item">
-            Библиотека VUElidate - для реактивной валидации полей различных форм
-          </li>
-          <li class="page__list-item">VUE-CLI - для сборки всего проекта</li>
-          <li class="page__list-item">
-            Библиотека компонентов Prime-VUE - для заимстовования некоторого
-            функционала
-          </li>
-          <li class="page__list-item">
-            Session Storage - для хранения данных об аутентифицированном
-            пользователе
-          </li>
-          <li class="page__list-item">
-            Indexed DataBase - для хранения данных о пользователях и задачах,
-            получаемых асинхронно при инициализации приложения и обновлямых при
-            действиях над пользователями и задачами путем ответов на мутации
-            глобального состояния
-          </li>
-        </ul>
-      </div>
     </div>
   </div>
 </template>
